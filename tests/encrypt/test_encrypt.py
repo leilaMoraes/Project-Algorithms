f692bd95024a67df1d1bc458a6eaa9bc70f43f15
Leila Moraes <leila.violino@gmail.com>
2023-06-01 17:30:27 -0300

    chore: lint

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,6 +1,7 @@
 from challenges.challenge_encrypt_message import encrypt_message
 import pytest
 
+
 def test_encrypt_message():
     assert encrypt_message("teste", 5) == "etset"
     assert encrypt_message("abcde", 2) == "edc_ba"
@@ -9,5 +10,3 @@ def test_encrypt_message():
         encrypt_message("abcde", "1")
     with pytest.raises(TypeError, match="tipo inválido para message"):
         encrypt_message(12345, 1)
-
-    
