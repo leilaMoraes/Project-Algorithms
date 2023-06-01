a2349e5645eda5caea4432835b7781360c517bcc
Leila Moraes <leila.violino@gmail.com>
2023-06-01 16:20:58 -0300

    chore: lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -14,7 +14,7 @@ def study_schedule(permanence_period, target_time):
             or period[1] < 0
             or period[0] > period[1]
         ):
-          return None
+            return None
         if period[0] <= target_time <= period[1]:
             count += 1
 
