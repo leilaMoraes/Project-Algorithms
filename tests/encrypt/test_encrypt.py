bc7cdb99bf0577eaa76b3e028e13bcc38c5ec58f
Leila Moraes <leila.violino@gmail.com>
2023-06-01 17:23:22 -0300

    test: implements tests for the encrypt_message function

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
-
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("teste", 5) == "etset"
+    assert encrypt_message("abcde", 2) == "edc_ba"
+    assert encrypt_message("abcde", 3) == "cba_ed"
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("abcde", "1")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(12345, 1)
+
+    
